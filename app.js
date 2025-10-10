080dcb23258901e89a5ca2721e0c4b2cf5cdff4f
thallys <thallys.paula.santos@escola.pr.gov.br>
2025-10-10 11:50:29 -0300

    save

--- app.js
+++ app.js
@@ -4,14 +4,24 @@
 // let paragrafo - document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag) {
    let campo = document.querySelector(tag);
    campo.innerHTML = Texto;
 }
 
-exibirTextoNaTela('h1')
-exibirTextoNaTela('p')
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verifcarChute() {
-    console.log(' O botão foi clicado!')
+    let chute = document.querySelector('input').value
+    
+    if (chute == numeroSecreto) {
+        
+    }
+}
+
+function gerarNumeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1);
 }
