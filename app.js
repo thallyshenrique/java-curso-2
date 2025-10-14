8f86d43ad30056be2ac0e0d1aecddbb38ee941b1
thallys <thallys.paula.santos@escola.pr.gov.br>
2025-10-14 11:03:10 -0300

    saveeee

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ function verifcarChute() {
     let chute = document.querySelector('input').value
     
     if (chute == numeroSecreto) {
-        
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    } else{
+      if (chute > numeroSecreto){
+         exibirTextoNaTela('p', O número secreto é menor);
+      } else {
+         exibirTextoNaTela('p', 'O número secreto é maior');
+      }
     }
 }
 
