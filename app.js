3f9d54a8ff11001d3938aadd27ca016a13436301
thallys <thallys.paula.santos@escola.pr.gov.br>
2025-10-16 09:07:47 -0300

    save

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag) {
    let campo = document.querySelector(tag);
@@ -15,20 +16,30 @@ exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verifcarChute() {
-    let chute = document.querySelector('input').value
+    let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+        let palavraTentativa = tentativas >1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = 'Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!';
+        exibirTextoNaTela('p', mensagemTentativas);
+        
     } else{
       if (chute > numeroSecreto){
-         exibirTextoNaTela('p', O número secreto é menor);
+         exibirTextoNaTela('p', 'O número secreto é menor');
       } else {
          exibirTextoNaTela('p', 'O número secreto é maior');
       }
+      tentativas++;
+      limparCampo();
     }
 }
 
 function gerarNumeroAleatorio() {
    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+   chute = document.querySelector('input');
+   chute.value = '';
 }
